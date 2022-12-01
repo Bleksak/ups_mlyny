161c8f82cff66d3456fe92b3c65759c669238088
Jiri Velek <jiri.velek7@protonmail.com>
2022-12-01 16:05:52 +0100

    deleted unnecessary files

--- server_rust/target/debug/deps/once_cell-3c5bcbc6d537eb21.d
+++
@@ -1,7 +0,0 @@
-/home/bleksak/ups_mlyny/server_rust/target/debug/deps/once_cell-3c5bcbc6d537eb21.rmeta: /home/bleksak/.cargo/registry/src/github.com-1ecc6299db9ec823/once_cell-1.16.0/src/lib.rs /home/bleksak/.cargo/registry/src/github.com-1ecc6299db9ec823/once_cell-1.16.0/src/imp_std.rs /home/bleksak/.cargo/registry/src/github.com-1ecc6299db9ec823/once_cell-1.16.0/src/race.rs
-
-/home/bleksak/ups_mlyny/server_rust/target/debug/deps/once_cell-3c5bcbc6d537eb21.d: /home/bleksak/.cargo/registry/src/github.com-1ecc6299db9ec823/once_cell-1.16.0/src/lib.rs /home/bleksak/.cargo/registry/src/github.com-1ecc6299db9ec823/once_cell-1.16.0/src/imp_std.rs /home/bleksak/.cargo/registry/src/github.com-1ecc6299db9ec823/once_cell-1.16.0/src/race.rs
-
-/home/bleksak/.cargo/registry/src/github.com-1ecc6299db9ec823/once_cell-1.16.0/src/lib.rs:
-/home/bleksak/.cargo/registry/src/github.com-1ecc6299db9ec823/once_cell-1.16.0/src/imp_std.rs:
-/home/bleksak/.cargo/registry/src/github.com-1ecc6299db9ec823/once_cell-1.16.0/src/race.rs:
