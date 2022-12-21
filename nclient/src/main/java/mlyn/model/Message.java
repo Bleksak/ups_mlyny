8dfbfece4e1224f032b3e994fc61de8dc774fed2
Jiri Velek <jiri.velek7@protonmail.com>
2022-12-22 00:24:32 +0100

    remove old client, go back to java 11

--- nclient/src/main/java/mlyn/model/Message.java
+++ nclient/src/main/java/mlyn/model/Message.java
@@ -2,14 +2,31 @@ package mlyn.model;
 
 import java.nio.ByteBuffer;
 
-public record Message(MessageType type, byte[] data) {
-    public byte[] serialize() {
-        int sizeInt = 2 * Integer.BYTES + (data == null ? 0 : data.length);
+public class Message {
 
-        if(data == null) {
-            return ByteBuffer.allocate(sizeInt).putInt(sizeInt).putInt(type.value()).array();
+    public MessageType _type;
+    public byte[] _data;
+
+    public Message(MessageType type, byte[] data) {
+        this._type = type;
+        this._data = data;
+    }
+
+    public MessageType type() {
+        return _type;
+    }
+
+    public byte[] data() {
+        return _data;
+    }
+
+    public byte[] serialize() {
+        int sizeInt = 2 * Integer.BYTES + (_data == null ? 0 : _data.length);
+
+        if(_data == null) {
+            return ByteBuffer.allocate(sizeInt).putInt(sizeInt).putInt(_type.value()).array();
         }
 
-        return ByteBuffer.allocate(sizeInt).putInt(sizeInt).putInt(type.value()).put(data).array();
+        return ByteBuffer.allocate(sizeInt).putInt(sizeInt).putInt(_type.value()).put(_data).array();
     }
 }
